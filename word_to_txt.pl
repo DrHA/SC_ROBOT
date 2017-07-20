a1a05a8503a12b192b90f0bd719a8b3ebcfc802a
DrHA <719520474@qq.COM>
2017-07-20 08:12:58 +0800

    word
    aa

---
+++ word_to_txt.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+#***************************************************
+#Author       :  DR.HA
+#Last modified:  15:09 2016/8/15
+#Email        :  719520474@qq.com
+#Filename     :  word_to_txt.pl
+#Description  :  把word文本内容提取并保存到txt
+#***************************************************
+use strict;
+use 
